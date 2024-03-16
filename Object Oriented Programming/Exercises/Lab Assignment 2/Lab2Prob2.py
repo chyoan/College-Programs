1eb3b99c607dc50fa53d98f6d2dc231b1e04d526
Hyo <138879492+chyoan@users.noreply.github.com>
2024-03-16 16:25:15 +0800

    Add files via upload

---
+++ Object Oriented Programming/Exercises/Lab Assignment 2/Lab2Prob2.py
@@ -0,0 +1,14 @@
+# Lab Exercise #2: Problem No. 2
+# Programmed by: Christian Kristopher D. Dalayoan
+# Course, Year and Section: BSCpE 1-3
+# Instructor: Engr. Julius S. Cansino
+# Date Submitted: March 16, 2024
+
+# Get user input
+name = input("Enter your full name: ")
+
+# Prints the result
+print(f"The whole name: {name}")
+print(f"The first character: {name[0]}")
+print(f"The first 5 characters: {name[:5]}")
+print(f"The fourth, fifth and sixth character: {name[3]}, {name[4]}, {name[5]} ")
