acef7fa6ae58ea8a2854cd65182fe0a0b928dfbc
Hyo <138879492+chyoan@users.noreply.github.com>
2024-03-17 00:11:09 +0800

    Create username_checker.py

---
+++ Object Oriented Programming/Exercises/Lab Assignment 2/username_checker.py
@@ -0,0 +1,14 @@
+# Lab Exercise #2: Problem No. 7
+# Programmed by: Christian Kristopher D. Dalayoan
+# Course, Year and Section: BSCpE 1-3
+# Instructor: Engr. Julius S. Cansino
+# Date Submitted: March 16, 2024
+
+# Get user input
+username = input("Enter username: ")
+
+# Checks the length of the input and prints the result
+if len(username) == 6:
+    print("You entered 6 characters.")
+else:
+    print("You did not enter 6 characters.")
